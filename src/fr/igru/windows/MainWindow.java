2bdb1cf4bf944269e9d1ed8e0bcfc93e00a92667
Thoringar <guillaumedelboulbes@gmail.com>
2020-01-06 18:05:21 -0500

    Essai ajout bouton

--- src/fr/igru/windows/MainWindow.java
+++ src/fr/igru/windows/MainWindow.java
@@ -1,5 +1,11 @@
 package fr.igru.windows;
 
+import javax.swing.*;
+
 public class MainWindow {
-    
+
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
 }
