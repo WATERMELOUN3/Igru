d0e2297a35b08210361829860ce9eddb7a550ea7
Guillaume Delboulbès <guillaumedelboulbes@gmail.com>
2020-01-07 19:37:24 +0100

    Finalisation UI Main Window (non cliquable) # Conflicts (supprimé): #	src/fr/igru/windows/ConnectionDialog.form #	src/fr/igru/windows/MainWindow.form #	src/fr/igru/windows/MainWindow.java

--- src/fr/igru/windows/MainWindow/MainWindow.java
+++ src/fr/igru/windows/MainWindow/MainWindow.java
@@ -20,7 +20,11 @@ public class MainWindow extends JPanel {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
         panel13 = new JPanel();
+        panel20 = new JPanel();
+        textField1 = new JTextField();
         panel14 = new JPanel();
+        panel21 = new JPanel();
+        textField2 = new JTextField();
         panel16 = new JPanel();
         panel17 = new JPanel();
         button9 = new JButton();
@@ -46,20 +50,17 @@ public class MainWindow extends JPanel {
         button6 = new JButton();
 
         //======== this ========
-        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder (
-        new javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e"
-        , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM
-        , new java. awt .Font ( "Dialo\u0067", java .awt . Font. BOLD ,12 )
-        ,java . awt. Color .red ) , getBorder () ) );  addPropertyChangeListener(
-        new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
-        ) { if( "borde\u0072" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
-        ;} } );
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+        EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing
+        . border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ),
+        java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (e -> {if ("\u0062ord\u0065r" .equals (e .getPropertyName () ))
+        throw new RuntimeException( ); });
         setLayout(new MigLayout(
             "hidemode 3",
             // columns
-            "[260,fill]" +
-            "[260,fill]" +
-            "[fill]",
+            "[235:260,fill]unrel" +
+            "[282:293,fill]" +
+            "[177,right]",
             // rows
             "[]" +
             "[]" +
@@ -82,7 +83,23 @@ public class MainWindow extends JPanel {
                 // columns
                 "[fill]",
                 // rows
-                "[225]"));
+                "0[]" +
+                "[199]"));
+
+            //======== panel20 ========
+            {
+                panel20.setLayout(new MigLayout(
+                    "fillx,hidemode 3",
+                    // columns
+                    "[fill]",
+                    // rows
+                    "0[]0"));
+
+                //---- textField1 ----
+                textField1.setText("                       Groupes                  ");
+                panel20.add(textField1, "cell 0 0,growx");
+            }
+            panel13.add(panel20, "cell 0 0");
         }
         add(panel13, "cell 0 0 1 12,aligny top,growy 0");
 
@@ -92,9 +109,25 @@ public class MainWindow extends JPanel {
             panel14.setLayout(new MigLayout(
                 "fill,hidemode 3",
                 // columns
-                "[fill]",
+                "[229:234,fill]",
                 // rows
-                "[179]"));
+                "0[]" +
+                "[155:154]"));
+
+            //======== panel21 ========
+            {
+                panel21.setLayout(new MigLayout(
+                    "fillx,hidemode 3",
+                    // columns
+                    "[fill]",
+                    // rows
+                    "0[]"));
+
+                //---- textField2 ----
+                textField2.setText("                             Utilisateurs              ");
+                panel21.add(textField2, "cell 0 0");
+            }
+            panel14.add(panel21, "cell 0 0");
         }
         add(panel14, "cell 1 0 1 12,aligny top,growy 0");
 
@@ -114,12 +147,12 @@ public class MainWindow extends JPanel {
                 panel17.setLayout(new MigLayout(
                     "fillx,hidemode 3",
                     // columns
-                    "[fill]",
+                    "[139:146,fill]",
                     // rows
                     "[]"));
 
                 //---- button9 ----
-                button9.setText("text");
+                button9.setText("Client Administrateur");
                 panel17.add(button9, "cell 0 0");
             }
             panel16.add(panel17, "cell 0 0");
@@ -134,7 +167,7 @@ public class MainWindow extends JPanel {
                     "[]"));
 
                 //---- button10 ----
-                button10.setText("text");
+                button10.setText("Sauvegarder Donn\u00e9es");
                 panel19.add(button10, "cell 0 0");
             }
             panel16.add(panel19, "cell 0 1");
@@ -149,7 +182,7 @@ public class MainWindow extends JPanel {
                     "[]"));
 
                 //---- button11 ----
-                button11.setText("text");
+                button11.setText("Visualisateur des tables");
                 panel18.add(button11, "cell 0 0");
             }
             panel16.add(panel18, "cell 0 2");
@@ -161,8 +194,8 @@ public class MainWindow extends JPanel {
             panel1.setLayout(new MigLayout(
                 "hidemode 3",
                 // columns
-                "[260,fill]" +
-                "[261,fill]" +
+                "[227:237,fill]unrel" +
+                "[274:297,fill]" +
                 "[260,fill]",
                 // rows
                 "[]0" +
@@ -171,9 +204,9 @@ public class MainWindow extends JPanel {
             //======== panel2 ========
             {
                 panel2.setLayout(new MigLayout(
-                    "fillx,hidemode 3",
+                    "hidemode 3",
                     // columns
-                    "[fill]" +
+                    "0[134:129,fill]unrel" +
                     "[fill]",
                     // rows
                     "0[]0"));
@@ -183,12 +216,13 @@ public class MainWindow extends JPanel {
                     panel4.setLayout(new MigLayout(
                         "fillx,hidemode 3",
                         // columns
-                        "[fill]",
+                        "0[120:n,fill]0",
                         // rows
                         "0[]0"));
 
                     //---- button1 ----
-                    button1.setText("text");
+                    button1.setText("Ajouter au groupe");
+                    button1.setIconTextGap(3);
                     panel4.add(button1, "cell 0 0,growx");
                 }
                 panel2.add(panel4, "cell 0 0");
@@ -198,12 +232,12 @@ public class MainWindow extends JPanel {
                     panel5.setLayout(new MigLayout(
                         "fillx,hidemode 3",
                         // columns
-                        "[fill]",
+                        "0[137,fill]0",
                         // rows
                         "0[]0"));
 
                     //---- button2 ----
-                    button2.setText("text");
+                    button2.setText("Enlever du groupe");
                     panel5.add(button2, "cell 0 0");
                 }
                 panel2.add(panel5, "cell 1 0");
@@ -225,12 +259,12 @@ public class MainWindow extends JPanel {
                     panel11.setLayout(new MigLayout(
                         "fillx,hidemode 3",
                         // columns
-                        "[fill]",
+                        "0[91,fill]0",
                         // rows
                         "[]"));
 
                     //---- button8 ----
-                    button8.setText("text");
+                    button8.setText("Ajouter");
                     panel11.add(button8, "cell 0 0");
                 }
                 panel10.add(panel11, "cell 0 0,growx");
@@ -245,7 +279,7 @@ public class MainWindow extends JPanel {
                         "[]"));
 
                     //---- button7 ----
-                    button7.setText("text");
+                    button7.setText("Supprimer");
                     panel12.add(button7, "cell 0 0");
                 }
                 panel10.add(panel12, "cell 1 0");
@@ -257,8 +291,8 @@ public class MainWindow extends JPanel {
                 panel7.setLayout(new MigLayout(
                     "fillx,hidemode 3",
                     // columns
-                    "[fill]" +
-                    "[fill]",
+                    "0[fill]unrel" +
+                    "[127:124,fill]0",
                     // rows
                     "0[]0"));
 
@@ -267,27 +301,27 @@ public class MainWindow extends JPanel {
                     panel8.setLayout(new MigLayout(
                         "fillx,hidemode 3",
                         // columns
-                        "[fill]",
+                        "0[133:118,left]0",
                         // rows
                         "[]"));
 
                     //---- button5 ----
-                    button5.setText("text");
-                    panel8.add(button5, "cell 0 0");
+                    button5.setText("Ajouter");
+                    panel8.add(button5, "cell 0 0,growx");
                 }
                 panel7.add(panel8, "cell 0 0");
 
                 //======== panel9 ========
                 {
                     panel9.setLayout(new MigLayout(
-                        "fillx,hidemode 3",
+                        "hidemode 3",
                         // columns
-                        "[fill]",
+                        "0[132:126,fill]0",
                         // rows
                         "[]"));
 
                     //---- button6 ----
-                    button6.setText("text");
+                    button6.setText("Supprimer");
                     panel9.add(button6, "cell 0 0");
                 }
                 panel7.add(panel9, "cell 1 0");
@@ -301,7 +335,11 @@ public class MainWindow extends JPanel {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - unknown
     private JPanel panel13;
+    private JPanel panel20;
+    private JTextField textField1;
     private JPanel panel14;
+    private JPanel panel21;
+    private JTextField textField2;
     private JPanel panel16;
     private JPanel panel17;
     private JButton button9;
