2ed4adc3b828147cf887a8fb40ef7cc5a3c396bd
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-01-09 19:24:43 +0100

    Ajout proprement de mysql connector

---
+++ src/fr/igruserver/Launcher.java
@@ -0,0 +1,10 @@
+package fr.igruserver;
+
+import fr.igruserver.windowsServer.MainServerWindow.MainServerWindow;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainServerWindow mainServerWindow = new MainServerWindow();
+        mainServerWindow.setVisible(true);
+    }
+}
