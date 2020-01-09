65e055404e772cf0b6f06cf7ad27811339d1a043
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-01-09 19:48:49 +0100

    Creation Launchers et tentative de faire fonctionner Swing, cet immondice

--- src/fr/igru/Launcher.java
+++ src/fr/igru/Launcher.java
@@ -1,13 +1,19 @@
 package fr.igru;
 
-import fr.igru.windowsclient.ConnectionDialog.connectionDialog;
+import fr.igru.windowsclient.ConnectionDialog.ConnectionDialog;
 import fr.igru.windowsclient.MainWindow.MainWindow;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Launcher {
     public static void main(String[] args) {
-        connectionDialog gui = new connectionDialog();
-        gui.setVisible(true);
+        JFrame ecran = new JFrame();
+        ecran.setMinimumSize(new Dimension(780, 400));
+
+        ConnectionDialog connectionDialog = new ConnectionDialog();
+        connectionDialog.setVisible(true);
+        ecran.add(connectionDialog);
+        ecran.setVisible(true);
     }
 }
--- src/fr/igru/windowsclient/ConnectionDialog/ConnectionDialog.java
+++ src/fr/igru/windowsclient/ConnectionDialog/ConnectionDialog.java
@@ -14,8 +14,8 @@ import java.awt.*;
 /**
  * @author unknown
  */
-public class connectionDialog extends JFrame {
-    public connectionDialog() {
+public class ConnectionDialog extends JPanel {
+    public ConnectionDialog() {
         initComponents();
     }
 
@@ -29,7 +29,7 @@ public class connectionDialog extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - unknown
+        // Generated using JFormDesigner Evaluation license - Alexis GELIN
         panel5 = new JPanel();
         panel10 = new JPanel();
         panel11 = new JPanel();
@@ -49,11 +49,13 @@ public class connectionDialog extends JFrame {
 
         //======== panel5 ========
         {
-            panel5.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
-            0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
-            . BOTTOM, new java. awt .Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
-            red ) ,panel5. getBorder () ) ); panel5. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
-            beans. PropertyChangeEvent e) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+            panel5.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax.
+            swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border
+            . TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog"
+            ,java .awt .Font .BOLD ,12 ), java. awt. Color. red) ,panel5. getBorder
+            ( )) ); panel5. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java
+            .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException
+            ( ); }} );
             panel5.setLayout(new MigLayout(
                 "fillx,hidemode 3",
                 // columns
@@ -191,7 +193,7 @@ public class connectionDialog extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - unknown
+    // Generated using JFormDesigner Evaluation license - Alexis GELIN
     private JPanel panel5;
     private JPanel panel10;
     private JPanel panel11;
