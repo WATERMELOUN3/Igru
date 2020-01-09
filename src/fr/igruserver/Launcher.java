50397f34b9dceb1ee061c0c035933d17e93219b5
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-01-09 19:48:49 +0100

    Creation Launchers et tentative de faire fonctionner Swing, cet immondice

--- src/fr/igruserver/Launcher.java
+++ src/fr/igruserver/Launcher.java
@@ -2,9 +2,17 @@ package fr.igruserver;
 
 import fr.igruserver.windowsServer.MainServerWindow.MainServerWindow;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Launcher {
     public static void main(String[] args) {
+        JFrame ecran = new JFrame();
+        ecran.setMinimumSize(new Dimension(780, 400));
+
         MainServerWindow mainServerWindow = new MainServerWindow();
         mainServerWindow.setVisible(true);
+        ecran.add(mainServerWindow);
+        ecran.setVisible(true);
     }
 }
