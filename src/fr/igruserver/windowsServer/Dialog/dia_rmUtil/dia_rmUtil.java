de29f9fb99f3b62497f3ec8415aa8ab633a9944a
Guillaume Delboulbès <guillaumedelboulbes@gmail.com>
2020-01-10 06:20:02 +0100

    Ajout des formulaires

--- src/fr/igruserver/windowsServer/Dialog/dia_rmUtil/dia_rmUtil.java
+++ src/fr/igruserver/windowsServer/Dialog/dia_rmUtil/dia_rmUtil.java
@@ -4,7 +4,10 @@
 
 package fr.igruserver.windowsServer.Dialog.dia_rmUtil;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import net.miginfocom.swing.*;
 
 /**
@@ -15,30 +18,99 @@ public class dia_rmUtil extends JPanel {
         initComponents();
     }
 
+    private void button1ActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void button2ActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
+        panel1 = new JPanel();
+        label1 = new JLabel();
+        panel4 = new JPanel();
+        label3 = new JLabel();
+        textFieldGrp = new JTextField();
+        panel3 = new JPanel();
+        buttonCancel = new JButton();
+        buttonConfirm = new JButton();
 
-        //======== this ========
-        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border
-        . EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax
-        . swing. border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,
-        12 ), java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans
-        . PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .
-        getPropertyName () )) throw new RuntimeException( ); }} );
-        setLayout(new MigLayout(
-            "hidemode 3",
-            // columns
-            "[fill]" +
-            "[fill]",
-            // rows
-            "[]" +
-            "[]" +
-            "[]"));
+        //======== panel1 ========
+        {
+            panel1.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder
+            ( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border
+            . TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt
+            . Color. red) ,panel1. getBorder( )) ); panel1. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void
+            propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( )
+            ; }} );
+            panel1.setLayout(new MigLayout(
+                "fill,hidemode 3",
+                // columns
+                "[220!,fill]",
+                // rows
+                "rel[25!]0" +
+                "[60!]" +
+                "[40!]"));
+
+            //---- label1 ----
+            label1.setText("Retirer un utilisateur");
+            panel1.add(label1, "cell 0 0,alignx center,growx 0");
+
+            //======== panel4 ========
+            {
+                panel4.setLayout(new MigLayout(
+                    "fill,hidemode 3",
+                    // columns
+                    "[64,fill]" +
+                    "[160,fill]",
+                    // rows
+                    "0[]0"));
+
+                //---- label3 ----
+                label3.setText("Username :");
+                panel4.add(label3, "cell 0 0,alignx left,growx 0");
+                panel4.add(textFieldGrp, "cell 1 0");
+            }
+            panel1.add(panel4, "cell 0 1");
+
+            //======== panel3 ========
+            {
+                panel3.setLayout(new MigLayout(
+                    "fill,hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "0[19:24]"));
+
+                //---- buttonCancel ----
+                buttonCancel.setText("Annuler");
+                buttonCancel.setBorder(new LineBorder(Color.red, 2, true));
+                buttonCancel.addActionListener(e -> button1ActionPerformed(e));
+                panel3.add(buttonCancel, "cell 0 0");
+
+                //---- buttonConfirm ----
+                buttonConfirm.setText("Confirmer");
+                buttonConfirm.addActionListener(e -> button2ActionPerformed(e));
+                panel3.add(buttonConfirm, "cell 1 0");
+            }
+            panel1.add(panel3, "cell 0 2");
+        }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - unknown
+    private JPanel panel1;
+    private JLabel label1;
+    private JPanel panel4;
+    private JLabel label3;
+    private JTextField textFieldGrp;
+    private JPanel panel3;
+    private JButton buttonCancel;
+    private JButton buttonConfirm;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
--- src/fr/igruserver/windowsServer/Dialog/dia_rmUtilFromGrp/dia_rmUtilFromGrp.java
+++ src/fr/igruserver/windowsServer/Dialog/dia_rmUtilFromGrp/dia_rmUtilFromGrp.java
@@ -4,7 +4,10 @@
 
 package fr.igruserver.windowsServer.Dialog.dia_rmUtilFromGrp;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import net.miginfocom.swing.*;
 
 /**
@@ -15,30 +18,124 @@ public class dia_rmUtilFromGrp extends JPanel {
         initComponents();
     }
 
+    private void button1ActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void button2ActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
+        panel1 = new JPanel();
+        label1 = new JLabel();
+        panel2 = new JPanel();
+        label2 = new JLabel();
+        textFieldUsername = new JTextField();
+        panel4 = new JPanel();
+        label3 = new JLabel();
+        textFieldGrp = new JTextField();
+        panel3 = new JPanel();
+        buttonCancel = new JButton();
+        buttonConfirm = new JButton();
 
-        //======== this ========
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.
-        border.EmptyBorder(0,0,0,0), "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e",javax.swing.border.TitledBorder.CENTER
-        ,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dialo\u0067",java.awt.Font
-        .BOLD,12),java.awt.Color.red), getBorder())); addPropertyChangeListener(
-        new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("borde\u0072"
-        .equals(e.getPropertyName()))throw new RuntimeException();}});
-        setLayout(new MigLayout(
-            "hidemode 3",
-            // columns
-            "[fill]" +
-            "[fill]",
-            // rows
-            "[]" +
-            "[]" +
-            "[]"));
+        //======== panel1 ========
+        {
+            panel1.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new
+            javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax
+            . swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java
+            .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt
+            . Color. red) ,panel1. getBorder( )) ); panel1. addPropertyChangeListener (new java. beans.
+            PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .
+            equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+            panel1.setLayout(new MigLayout(
+                "fill,hidemode 3",
+                // columns
+                "[220!,fill]",
+                // rows
+                "rel[25!]0" +
+                "[40!]" +
+                "[40!]" +
+                "[40!]"));
+
+            //---- label1 ----
+            label1.setText("Retirer Utilisateur \u00e0 un groupe");
+            panel1.add(label1, "cell 0 0,alignx center,growx 0");
+
+            //======== panel2 ========
+            {
+                panel2.setLayout(new MigLayout(
+                    "fill,hidemode 3",
+                    // columns
+                    "[53:64,fill]" +
+                    "[137,fill]",
+                    // rows
+                    "rel[]0"));
+
+                //---- label2 ----
+                label2.setText("Username :");
+                panel2.add(label2, "cell 0 0,alignx left,growx 0");
+                panel2.add(textFieldUsername, "cell 1 0");
+            }
+            panel1.add(panel2, "cell 0 1");
+
+            //======== panel4 ========
+            {
+                panel4.setLayout(new MigLayout(
+                    "fill,hidemode 3",
+                    // columns
+                    "[64,fill]" +
+                    "[160,fill]",
+                    // rows
+                    "0[]0"));
+
+                //---- label3 ----
+                label3.setText("Groupe :");
+                panel4.add(label3, "cell 0 0,alignx left,growx 0");
+                panel4.add(textFieldGrp, "cell 1 0");
+            }
+            panel1.add(panel4, "cell 0 2");
+
+            //======== panel3 ========
+            {
+                panel3.setLayout(new MigLayout(
+                    "fill,hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "0[19:24]"));
+
+                //---- buttonCancel ----
+                buttonCancel.setText("Annuler");
+                buttonCancel.setBorder(new LineBorder(Color.red, 2, true));
+                buttonCancel.addActionListener(e -> button1ActionPerformed(e));
+                panel3.add(buttonCancel, "cell 0 0");
+
+                //---- buttonConfirm ----
+                buttonConfirm.setText("Confirmer");
+                buttonConfirm.addActionListener(e -> button2ActionPerformed(e));
+                panel3.add(buttonConfirm, "cell 1 0");
+            }
+            panel1.add(panel3, "cell 0 3");
+        }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - unknown
+    private JPanel panel1;
+    private JLabel label1;
+    private JPanel panel2;
+    private JLabel label2;
+    private JTextField textFieldUsername;
+    private JPanel panel4;
+    private JLabel label3;
+    private JTextField textFieldGrp;
+    private JPanel panel3;
+    private JButton buttonCancel;
+    private JButton buttonConfirm;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
